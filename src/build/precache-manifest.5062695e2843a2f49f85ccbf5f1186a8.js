5bae7b618a989b4cad47c86df7603ed739cccb1b
Amit Gupta <amgupta@mafcarrefour.com>
2019-10-10 15:45:56 +0400

    First commit

---
+++ src/build/precache-manifest.5062695e2843a2f49f85ccbf5f1186a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98b24ae58e60a7ba441bebd5163b091a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a8f279c3239217d8aed",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "fafc35037569732c4ca3",
+    "url": "/static/js/2.9969faec.chunk.js"
+  },
+  {
+    "revision": "3a8f279c3239217d8aed",
+    "url": "/static/js/main.4932330d.chunk.js"
+  },
+  {
+    "revision": "c8e3c18ef3cf4558092d",
+    "url": "/static/js/runtime-main.aa8267cb.js"
+  }
+]);
